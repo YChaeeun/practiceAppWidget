fa1b3b7d2fc21981ec9cfe28010620daae8890a3
YChaeeun <ychae.leah@gmail.com>
2020-06-26 15:36:11 +0900

    feat-#10 갤러리에서 이미지 선택하여 uri 가져오기
    - 여러 개의 이미지를 가져오는 건 API 18이상만 가능 흠..

--- app/src/main/java/com/example/appwidget/MainActivity.kt
+++ app/src/main/java/com/example/appwidget/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.appwidget
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.AdapterViewFlipper
+import android.widget.Toast
 import androidx.annotation.DrawableRes
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : PermissionActivity() {
 
@@ -49,8 +54,53 @@ class MainActivity : PermissionActivity() {
         btn_toggle.setOnClickListener {
             //if (btn_toggle.isChecked) af.startFlipping() else af.stopFlipping()
             openMediaStore()
+            pickImages()
         }
     }
 
+    private fun pickImages() {
+        Intent().apply {
+            putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)  // API 18 이상 사용 가능
+            type = "image/*"
+            action = Intent.ACTION_GET_CONTENT
+            //action = Intent.ACTION_PICK
+            startActivityForResult(Intent.createChooser(this, "Select Picture"), PICK_IMAGE)
+        }
+    }
 
+    private fun openGallery() {
+        val intentGallery = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+        startActivity(intentGallery)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            val uriList = mutableListOf<Uri>()
+
+            // select one item
+            data?.data?.let {
+                uriList.add(it)
+            }
+
+            //select multiple item
+            data?.clipData?.let {
+                for (i in 0 until it.itemCount) {
+                    uriList.add(it.getItemAt(i).uri)
+                }
+            }
+
+            Toast.makeText(this@MainActivity, uriList.toString(), Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+
+    companion object {
+        const val PICK_IMAGE = 100
+    }
 }
